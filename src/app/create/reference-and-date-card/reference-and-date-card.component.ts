cbf7dad34f187b1ec93dbe22897b4a5018a60f77
Tarek Jellali <jellalitarek@gmail.com>
2019-07-06 16:22:35 +0200

    Add reference card for the invoice

--- src/app/create/reference-and-date-card/reference-and-date-card.component.ts
+++ src/app/create/reference-and-date-card/reference-and-date-card.component.ts
@@ -1,17 +1,48 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Output,
+  Input,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges
+} from "@angular/core";
 import { ReferenceAndDate } from "src/app/models/reference-and-date.model";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-reference-and-date-card",
   templateUrl: "./reference-and-date-card.component.html",
   styleUrls: ["./reference-and-date-card.component.scss"]
 })
-export class ReferenceAndDateCardComponent implements OnInit {
+export class ReferenceAndDateCardComponent implements OnInit, OnChanges {
+  @Input()
   public refAndDate: ReferenceAndDate = {
     reference: "00001",
     date: new Date(Date.now())
   };
-  constructor() {}
 
-  ngOnInit() {}
+  @Output()
+  public refAndDateChange = new EventEmitter<ReferenceAndDate>();
+
+  public dateForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.dateForm = this.formBuilder.group({
+      date: [null, Validators.required]
+    });
+    this.dateForm.controls.date.setValue(this.refAndDate.date);
+    this.dateForm.controls.date.valueChanges.subscribe(dateValue => {
+      this.refAndDate.date = dateValue;
+      this.refAndDateChange.emit(this.refAndDate);
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.refAndDate !== undefined) {
+      this.refAndDate = changes.refAndDate.currentValue;
+    }
+  }
 }
