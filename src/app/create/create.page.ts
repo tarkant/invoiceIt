929cb66ed3213c345ce900a1b8543ef0937eae49
Tarek Jellali <jellalitarek@gmail.com>
2019-07-31 22:48:13 +0200

    Add firestore and fix unit tests

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.page.html',
-  styleUrls: ['./create.page.scss'],
+  selector: "app-create",
+  templateUrl: "./create.page.html",
+  styleUrls: ["./create.page.scss"]
 })
 export class CreatePage implements OnInit {
+  private invoicesList: AngularFirestoreCollection<any>;
+  item: Observable<any>;
 
-  constructor() { }
+  constructor(private angularFireStore: AngularFirestore) {}
 
   ngOnInit() {
+    this.invoicesList = this.angularFireStore.collection<any>("invoices");
+    this.item = this.invoicesList.valueChanges();
+    this.item.subscribe(val => console.log(val));
   }
-
 }
---
+++ src/app/shared/services/main.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+
+/**
+ * Fetches for the app the basic needed data such as :
+ * * The settings
+ * * The invoices list
+ */
+@Injectable({
+  providedIn: "root"
+})
+export class MainService {
+  constructor(private fire) {}
+}
