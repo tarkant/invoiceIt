88a8e8e653a3ab07e997193130cd49cc10ffdac7
Tarek Jellali <jellalitarek@gmail.com>
2019-08-04 21:16:22 +0200

    Add some routes for the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
+      title: "Accueil",
+      url: "/home",
+      icon: "home"
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: "Liste",
+      url: "/list",
+      icon: "list"
+    },
+    {
+      title: "Créer une facture/devis",
+      url: "/create",
+      icon: "add-circle"
+    },
+    {
+      title: "Clients",
+      url: "/clients",
+      icon: "people"
     }
   ];
 
